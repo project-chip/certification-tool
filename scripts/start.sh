7c42c40f33fb940e9ed6888ec302b9b3c776b2ad
Hilton Lima <hilton_lima@apple.com>
2024-03-25 15:49:53 +0000

    some fixes

--- scripts/start.sh
+++ scripts/start.sh
@@ -30,11 +30,12 @@ set -e
 ./scripts/install-default-env.sh
 
 # Dev override files
-BACKEND_COMPOSE="-f docker-compose.override-backend-dev.yml"
-FRONTEND_COMPOSE="-f docker-compose.override-frontend-dev.yml"
+BACKEND_COMPOSE_DEV="-f docker-compose.override-backend-dev.yml"
+FRONTEND_COMPOSE_DEV="-f docker-compose.override-frontend-dev.yml"
 
 # Parse args for which docker compose overrides to use
-DEV_COMPOSE_FILES=""
+BACKEND_COMPOSE=""
+FRONTEND_COMPOSE=""
 COMPOSE_CACHE_OPTION=""
 BACKEND_DEV=false
 FRONTEND_DEV=false
@@ -43,12 +44,12 @@ do
     case $arg in
         -b|--backend-dev)
         BACKEND_DEV=true
-        DEV_COMPOSE_FILES="$DEV_COMPOSE_FILES $BACKEND_COMPOSE"
+        BACKEND_COMPOSE=$BACKEND_COMPOSE_DEV
         shift # Remove --backend from processing
         ;;
         -f|--frontend-dev)
         FRONTEND_DEV=true
-        DEV_COMPOSE_FILES="$DEV_COMPOSE_FILES $FRONTEND_COMPOSE"
+        FRONTEND_COMPOSE=$FRONTEND_COMPOSE_DEV
         shift # Remove --frontend from processing
         ;;
         *)
@@ -58,8 +59,38 @@ do
     esac
 done
 
-# Start docker containers with docker-compose
-docker compose -f docker-compose.yml $DEV_COMPOSE_FILES up -d
+# Do not exit in case of error
+set +e
+
+echo "*** Starting 'db' and 'proxy' docker containers"
+DOCKER_UP="docker compose -f docker-compose.yml up db proxy --detach"
+$DOCKER_UP
+if [ $? -ne 0 ]; then
+    echo "### Exit with Error ###"
+    echo "    Unable to start containers."
+    echo "    You can also pull the images manually using this command: $DOCKER_UP"
+    exit 1
+fi
+
+echo "*** Starting 'backend' docker container"
+docker compose -f docker-compose.yml $BACKEND_COMPOSE up backend --detach --no-build
+if [ $? -ne 0 ]; then
+    echo "### Exit with Error ###"
+    echo "    Unable to start backend container."
+    echo "    Check if the tag is correct in the docker compose file."
+    echo "    You can also build the image manually using this command: $ './backend/scripts/build-docker-image.sh'"
+    exit 1
+fi
+
+echo "*** Starting 'frontend' docker container"
+docker compose -f docker-compose.yml $FRONTEND_COMPOSE up frontend --detach --no-build
+if [ $? -ne 0 ]; then
+    echo "### Exit with Error ###"
+    echo "    Unable to start frontend container."
+    echo "    Check if the tag is correct in the docker compose file."
+    echo "    You can also build the image manually using this command: $ './frontend/scripts/build-docker-image.sh'"
+    exit 1
+fi
 
 if [ "$FRONTEND_DEV" = true ] ; then
     echo "!!!! Matter TH frontend started in development mode."
@@ -69,7 +100,7 @@ else
     until docker compose exec frontend curl --fail -s --output /dev/null http://localhost:4200
     do
         echo -n "."
-	sleep 5
+	    sleep 5
     done
     echo " done"
 fi
@@ -82,7 +113,7 @@ else
     until docker compose exec backend curl --fail -s --output /dev/null http://localhost/docs
     do
         echo -n "."
-	sleep 5
+	    sleep 5
     done
     echo " done"
 fi
